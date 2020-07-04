a422003f8349c2eeb779fed8e1156d247284f83a
BekkaliyevAssan <marina.neymar.13@gmail.com>
2020-07-04 20:35:18 +0500

    hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SvgComponent } from './components/svg/svg.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SvgComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
