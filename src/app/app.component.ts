c448ccec86d1477fece21acda9b47c4d070bdd37
BekkaliyevAssan <marina.neymar.13@gmail.com>
2020-07-07 18:16:22 +0500

    login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentChecked } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentChecked {
+  public path
   title = 'renta';
+
+  ngAfterContentChecked() {  
+    let arr = (window.location.href.split('/'))
+    this.path = arr[arr.length - 1]  
+    console.log(this.path)
+  }
 }
