f24fb064d41e23e7ec9ea840d9d9bb3a49b8ef20
BekkaliyevAssan <marina.neymar.13@gmail.com>
2020-08-13 22:13:16 +0500

    new update: prod details

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -12,11 +12,17 @@ export class ProductDetailsComponent implements OnInit {
   constructor(private route: ActivatedRoute, private productService: ProductService) { }
   public id
   public prod
+  public loader = true
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('product_id')
     this.productService.getProductInfo(this.id).subscribe(data => {
     this.prod = data
+    console.log(this.prod)
     })
+  }
+
+  ngAfterViewInit() {
+    this.loader = false
   }
 
 }
