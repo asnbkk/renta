9e076cebc5d4994603b821ec5305f06b31ac7899
y4m4t0 <argun63901@gmail.com>
2020-10-11 14:51:17 +0600

    env.ts is now not in .gitignore

---
+++ src/assets/env.ts
@@ -0,0 +1,12 @@
+let root_url = 'http://localhost:3000/'
+let api_url = 'http://localhost:3000/'
+
+// let root_url = 'https://arend.kz/'
+// let api_url = 'https://arend.kz/'
+
+
+    
+export { 
+    api_url,
+    root_url
+}
